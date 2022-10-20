533d2321ecf78b85a417a287b68be3eebba8e4d5
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-10-20 09:42:48 +0100

    Add and style nav bar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Login from "./pages/Login";
 function App() {
   return (
       <Router>
+          <nav>
+              <Link to="/">Home</Link>
+              <Link to="/createpost">Create Post</Link>
+              <Link to="/login">Login</Link>
+          </nav>
         <Routes>
           <Route path="/" element={<Home />} />
             <Route path="/createpost" element={<CreatePost />} />
